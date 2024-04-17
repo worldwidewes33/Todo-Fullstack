8abd73af6574537721a2c1ac3a323cff2206cfb0
Wesley Austin <131537918+worldwidewes33@users.noreply.github.com>
2024-04-17 11:57:27 -0600

    connected to mongodb atlas database

--- server/app.js
+++ server/app.js
@@ -6,6 +6,16 @@ dotenv.config({ path: "./config.env" });
 
 const app = express();
 
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB)
+  .then(() => console.log("connected to database"))
+  .catch((err) => console.log(err));
+
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
