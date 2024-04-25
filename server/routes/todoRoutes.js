55f3a06b5258f92ffb7eeabeade4d1addd8df1a2
Wesley Austin <131537918+worldwidewes33@users.noreply.github.com>
2024-04-24 19:16:38 -0600

    implemented todo routes

---
+++ server/routes/todoRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const todoController = require("../controllers/todoController");
+
+const router = express.Router();
+
+router
+  .route("/")
+  .get(todoController.getAllTodos)
+  .post(todoController.createTodo);
+
+router
+  .route("/:id")
+  .get(todoController.getTodo)
+  .delete(todoController.deleteTodo)
+  .patch(todoController.updateTodo);
+
+module.exports = router;
