54a575c2d6eb6247979104d4374b3659cb8b464c
Wesley Austin <131537918+worldwidewes33@users.noreply.github.com>
2024-04-24 19:17:47 -0600

    added todos api route

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
+const todoRoutes = require("./routes/todoRoutes");
+
 dotenv.config({ path: "./config.env" });
 
 const app = express();
@@ -17,6 +19,10 @@ mongoose
   .catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+
+app.use("/api/todos", todoRoutes);
 
 app.listen(PORT, () => {
   console.log(`server listening on ${PORT}`);
