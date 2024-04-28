f659ce4dd27b7ca0a89b6a229f65842ee6a52eb8
Wesley Austin <131537918+worldwidewes33@users.noreply.github.com>
2024-04-28 10:17:18 -0600

    built todo list form and display

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,30 @@
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div className="container">
+      <h1>Todo List</h1>
+      <form className="form">
+        <input type="text" placeholder="Add todo item" />
+        <button type="submit">Add</button>
+      </form>
+      <div className="todo-list">
+        <div className="todo-item">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <button className="btn-update">Update</button>
+          <button className="btn-delete">Delete</button>
+        </div>
+        <div className="todo-item">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <button className="btn-update">Update</button>
+          <button className="btn-delete">Delete</button>
+        </div>
+        <div className="todo-item">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <button className="btn-update">Update</button>
+          <button className="btn-delete">Delete</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
