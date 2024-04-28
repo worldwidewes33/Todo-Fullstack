d9832137633aaea765d10564a87e68cc2099e8a7
Wesley Austin <131537918+worldwidewes33@users.noreply.github.com>
2024-04-28 16:30:34 -0600

    added cors

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const todoRoutes = require("./routes/todoRoutes");
 
@@ -20,6 +21,7 @@ mongoose
 
 const PORT = process.env.PORT || 8000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/todos", todoRoutes);
